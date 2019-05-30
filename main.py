7343a589d7d5b227a7f0283fec51f82716446cb1
yellowlmf1981 <907784617@qq.com>
2019-05-30 17:30:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print('**************master adding ******************')
 print('**************adding now*******************')
 print('**************adding from master branch')
 print('**************adding from pythonD branch*************')
-print('***************adding at 17:28***************')
+print('***************adding at 17:28**')
 
 
 
